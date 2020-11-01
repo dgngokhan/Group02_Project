8c784f7ba626eea7f65d9ab8ddb712fd11e201ba
Dawud_SF <dawudabduweli@gmail.com>
2020-11-01 13:48:16 -0800

    deleted something --Local(Dawud)

--- src/main/java/Dawud.java
+++ src/main/java/Dawud.java
@@ -3,12 +3,6 @@ public class Dawud {
         System.out.println("Commit from IntelliJ");
         System.out.println("Second Commit from Dawud");
         // change from branch-Dawud
-
-            System.out.println("Company Name: Uber");
-            System.out.println("Employee Name: Dawud");
-            System.out.println("Employee ID: 12345");
-            System.out.println("Job Title: SDET");
-            System.out.println("Salary: $140000");
             // I'm adding fifth change from branch-Dawud
             // I keep adding change
 
