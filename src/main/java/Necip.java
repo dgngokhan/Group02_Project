a4f515996e2ffe13d329676192c8ef907e65491e
Dawud_SF <dawudabduweli@gmail.com>
2020-11-01 12:40:22 -0800

    Fourth Commit Messing up with Necip's class --from local(Dawud)

--- src/main/java/Necip.java
+++ src/main/java/Necip.java
@@ -1,5 +1,10 @@
 public class Necip {
     public static void main(String[] args) {
         System.out.println("You are awesome");
+        System.out.println("Company Name: Google");
+        System.out.println("Employee Name: Necip");
+        System.out.println("Employee ID: 12340");
+        System.out.println("Job Title: SDET");
+        System.out.println("Salary: $500000");
     }
 }
