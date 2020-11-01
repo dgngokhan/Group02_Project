cb94ca89ca0be0170eebd827aa5cd6657b4b515d
dgngokhan <67300102+dgngokhan@users.noreply.github.com>
2020-11-01 13:28:36 -0700

    Job offer for Dawud

--- src/main/java/Dawud.java
+++ src/main/java/Dawud.java
@@ -1,6 +1,11 @@
 public class Dawud {
     public static void main(String[] args) {
-        System.out.println("Commit from IntelliJ");
+            System.out.println("Company Name: Uber");
+            System.out.println("Employee Name: Dawud");
+            System.out.println("Employee ID: 12345");
+            System.out.println("Job Title: SDET");
+            System.out.println("Salary: $140000");
 
-    }
+
+        }
 }
