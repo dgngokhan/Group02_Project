6cd77193a8c83c8200a5b47ebf4ade93053d028f
Dawud_SF <dawudabduweli@gmail.com>
2020-11-01 12:24:22 -0800

    Second Commit from local(Dawud)

--- src/main/java/Dawud.java
+++ src/main/java/Dawud.java
@@ -1,6 +1,7 @@
-public class Dawud {
+public class Dawud{
     public static void main(String[] args) {
         System.out.println("Commit from IntelliJ");
+        System.out.println("Second Commit from Dawud");
 
     }
 }
