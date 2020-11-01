4fef7109bd296e0652f15efbd50e4bda77fb6219
Dawud_SF <dawudabduweli@gmail.com>
2020-11-01 12:32:52 -0800

    Third Commit from Dawud branch--from local(Dawud)

--- src/main/java/Dawud.java
+++ src/main/java/Dawud.java
@@ -2,6 +2,7 @@ public class Dawud{
     public static void main(String[] args) {
         System.out.println("Commit from IntelliJ");
         System.out.println("Second Commit from Dawud");
+        // change from branch-Dawud
 
     }
 }
