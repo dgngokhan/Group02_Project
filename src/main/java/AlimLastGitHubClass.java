c67c015ff072fe0989d27673d4393cfcd07fabc7
alim281 <alim281@gmail.com>
2020-11-01 13:35:35 -0800

    LastGitHubClass.Alim

--- src/main/java/AlimLastGitHubClass.java
+++ src/main/java/AlimLastGitHubClass.java
@@ -1,6 +1,6 @@
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
-public class LastGitHubClass {
+public class AlimLastGitHubClass {
     public static void main(String[] args) {
         System.out.print("Last gitHub Class");
         System.out.println("See you soon");
